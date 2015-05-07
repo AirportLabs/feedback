b4790642cfdb7974839e83c74a3b84f231077f77
Christian Kessler IV <me@cgk.io>
2015-05-07 17:29:05 -0400

    Updates

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ionic-timepicker', 'angularMoment'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -170,6 +170,26 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   })
 
+  .state('tab.q26', {
+    url: '/q26',
+    views: {
+      'tab-give': {
+        templateUrl: 'templates/tab-Q26.html',
+        controller: 'Q26'
+      }
+    }
+  })
+
+  .state('tab.q27', {
+    url: '/q27',
+    views: {
+      'tab-give': {
+        templateUrl: 'templates/tab-Q27.html',
+        controller: 'Q27'
+      }
+    }
+  })
+
   .state('tab.done', {
     url: '/done',
     views: {
@@ -194,4 +214,89 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/q1');
 
+})
+
+.directive('standardTimeMeridian', function() {
+  return {
+    restrict: 'AE',
+    replace: true,
+    scope: {
+      etime: '=etime'
+    },
+    template: "<strong>{{stime}}</strong>",
+    link: function(scope, elem, attrs) {
+
+      scope.stime = epochParser(scope.etime, 'time');
+
+      function prependZero(param) {
+        if (String(param).length < 2) {
+          return "0" + String(param);
+        }
+        return param;
+      }
+
+      function epochParser(val, opType) {
+        if (val === null) {
+          return "00:00";
+        } else {
+          var meridian = ['AM', 'PM'];
+
+          if (opType === 'time') {
+            var hours = parseInt(val / 3600);
+            var minutes = (val / 60) % 60;
+            var hoursRes = hours > 12 ? (hours - 12) : hours;
+
+            var currentMeridian = meridian[parseInt(hours / 12)];
+
+            return (prependZero(hoursRes) + ":" + prependZero(minutes) + " " + currentMeridian);
+          }
+        }
+      }
+
+      scope.$watch('etime', function(newValue, oldValue) {
+        scope.stime = epochParser(scope.etime, 'time');
+      });
+
+    }
+  };
+})
+
+.directive('standardTimeNoMeridian', function() {
+  return {
+    restrict: 'AE',
+    replace: true,
+    scope: {
+      etime: '=etime'
+    },
+    template: "<strong>{{stime}}</strong>",
+    link: function(scope, elem, attrs) {
+
+      scope.stime = epochParser(scope.etime, 'time');
+
+      function prependZero(param) {
+        if (String(param).length < 2) {
+          return "0" + String(param);
+        }
+        return param;
+      }
+
+      function epochParser(val, opType) {
+        if (val === null) {
+          return "00:00";
+        } else {
+          if (opType === 'time') {
+            var hours = parseInt(val / 3600);
+            var minutes = (val / 60) % 60;
+
+            return (prependZero(hours) + ":" + prependZero(minutes));
+          }
+        }
+      }
+
+      scope.$watch('etime', function(newValue, oldValue) {
+        scope.stime = epochParser(scope.etime, 'time');
+      });
+
+    }
+  };
 });
